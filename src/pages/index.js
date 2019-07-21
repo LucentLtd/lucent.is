2a8978a65972e8bb136d43ab1d143bfc2a418c91
Nicholas Rempel <nbrempel@gmail.com>
2019-07-20 18:38:24 -0700

    no shadow
    Signed-off-by: Nicholas Rempel <nbrempel@gmail.com>

--- src/pages/index.js
+++ src/pages/index.js
@@ -124,7 +124,7 @@ const Index = ({}) => {
                 </div>
                 <div className="flex items-center justify-between">
                     <button
-                        className="bg-lucent-blue text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                        className="bg-lucent-blue text-white font-bold py-2 px-4 rounded focus:outline-none"
                         type="submit"
                     >
                         Send
