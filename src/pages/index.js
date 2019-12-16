b017c05085e154c795ca89b623b4baeab8ce5949
Nicholas Rempel <nbrempel@gmail.com>
2019-12-16 10:45:57 -1000

    more copy
    Signed-off-by: Nicholas Rempel <nbrempel@gmail.com>

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,12 +10,13 @@ const Index = ({}) => {
             <h1 className="visuallyhidden">Lucent Enterprises Ltd.</h1>
             <h2 id="about">We Are Lucent!</h2>
             <p>
-                We are a collaborative marketer and product-minded software
-                creator duo who believe in making a positive impact with
-                whatever we do. Between the two of us, we have planned and
-                hosted large events, produced dance performances, run company
-                marketing efforts, led small engineering teams, and built
-                software products used by thousands of people.
+                We are <b>Lucent Enterprises</b>. We're a collaborative marketer
+                and product-minded software creator duo who believe in making a
+                positive impact with whatever we do. Between the two of us, we
+                have planned and hosted large events, produced dance
+                performances, run company marketing efforts, led small
+                engineering teams, and built software products used by thousands
+                of people.
             </p>
             <p>
                 We want to give back to our community by participating in
@@ -25,27 +26,39 @@ const Index = ({}) => {
             </p>
             <h2 id="work">Our Work</h2>
             <p>
-                Electra Productions is a dance production company founded by
-                Linley Faulkner and Jane Bull. The’ve produced multiple sold-out
-                dance performances in Victoria, BC that attracted performers
-                from across Canada and the United States.
+                <a href="https://www.poleart.ca">Electra Productions</a> is a
+                dance production company founded by Linley Faulkner and Jane
+                Bull. The’ve produced multiple sold-out dance performances in
+                Victoria, BC that attracted performers from across Canada and
+                the United States.
             </p>
             <p>
-                30 Hour Jobs is a job board and community for job seekers who
-                need or want to work less than 40 hours per week and employers
-                who support this. Nick created 30 Hour Jobs with the belief
-                that, while many people will work more than 30 hours per week,
-                the number of hours dedicated to an employer alone should be
-                less than it is currently. By reducing the expected time
-                commitment to employers, individuals will have more time to
-                raise their families, engage in their community, work on their
-                passions, and do great things.
+                <a href="https://30hourjobs.com">30 Hour Jobs</a> is a job board
+                and community for job seekers who need or want to work less than
+                40 hours per week and employers who support this. Nick created
+                30 Hour Jobs with the belief that, while many people will work
+                more than 30 hours per week, the number of hours dedicated to an
+                employer alone should be less than it is currently. By reducing
+                the expected time commitment to employers, individuals will have
+                more time to spend time with their families, engage in their
+                community, work on their passions, and do great things.
             </p>
             <p>
-                Nick currently works primarily as a contract software developer
-                who is available to hire. He has experience creating scalable
-                web software solutions as well as creating cross-platform mobile
-                applications for Android and iOS.
+                <a href="https://github.com/nrempel">Nick</a> is an experienced
+                freelance software developer who is{" "}
+                <a href="mailto:nick@lucent.is">available to hire</a>. He has
+                experience creating{" "}
+                <a href="https://vonx.io">scalable web software solutions</a> as
+                well as creating{" "}
+                <a href="https://www.ferriesapp.com">
+                    cross-platform mobile applications
+                </a>{" "}
+                for Android and iOS.
+            </p>
+            <p>
+                <a href="https://ca.linkedin.com/in/linleyf"></a>Linley is an
+                experienced writer and marketer who can help you scale your
+                business.
             </p>
             <h2 id="people">Who We Are</h2>
             <h3>Linley Faulkner</h3>
@@ -54,17 +67,13 @@ const Index = ({}) => {
                     className="w-32 mt-1 mr-3 mb-3 rounded float-left shadow"
                     src="linley.jpg"
                 />
-                Sint anim ullamco irure sit magna aliqua laborum ut dolore.
-                Veniam veniam ut Lorem tempor mollit aute. Deserunt incididunt
-                do dolor adipisicing magna pariatur irure laboris sit excepteur
-                aute eu amet. Eu mollit cupidatat qui occaecat do eiusmod
-                consequat excepteur commodo. Deserunt culpa veniam minim sunt
-                cillum deserunt veniam. Sint nulla dolore non non ipsum
-                incididunt id duis laboris irure qui. Sunt sint elit minim culpa
-                quis ullamco culpa laborum dolore cillum sunt pariatur nisi. Do
-                tempor anim dolor anim Lorem laborum aute ullamco aute in.
-                Excepteur voluptate excepteur eu fugiat sint incididunt sint
-                Lorem sunt dolor velit sint.
+                Linley is experienced writer, marketer, and leader and she has
+                experience coordinating recruiting and sales processes for small
+                to medium sized businesses. She is passionate about engaging
+                with her community and she works hard to leave a positive impact
+                with the work that she does. Before working in the technology
+                sector, Linley worked with not for profit organizations in
+                Victoria and British Columbia.
             </p>
             <h3>Nicholas Rempel</h3>
             <p>
@@ -77,7 +86,7 @@ const Index = ({}) => {
                 a software company whose product enabled restaurant owners to
                 engage their customers by SMS message after their meal. There,
                 he acted as CTO and Head of Software. Nick has experience
-                leading small engineering teams and has experience designing and
+                leading small engineering teams as well as designing and
                 implemented software projects used by thousands of people.
                 Before working with technology, Nick worked with individuals
                 with Autism spectrum disorder to support them in daily life.
